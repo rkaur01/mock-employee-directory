5e21a40715e8cfe5de16607404cf5fe8ea50a5af
rkaur01 <kaur.rasprit@gmail.com>
2019-08-10 02:33:04 -0400

    wip edit method

--- src/App.js
+++ src/App.js
@@ -52,20 +52,27 @@ export default class App extends Component {
     }
   }
 
+  //@TODO: make edit and add DRY, differences are only in method, state btnClicked, and json passed in
   editEmployee(updatedEmployee) {
-    // find employee by updatedEmployee.id and set that object in state to updatedEmployee data
-    let employees = this.state.employees;
-    const foundIndex = employees.findIndex(emp => emp.id === updatedEmployee.id);
-    
-    employees[foundIndex] = updatedEmployee;
-    this.setState({
-      employees,
+    let firstName = updatedEmployee.firstName.value,
+      lastName = updatedEmployee.lastName.value,
+      job = updatedEmployee.jobTitle.value,
+      email = updatedEmployee.email.value;
+
+    fetch('http://localhost:3001/employees', {
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      method: 'PUT',
+      body: JSON.stringify({firstName,lastName,job,email})
+    })
+    .then(this.handleErrors)
+    .then(response => this.setState({
+      staleData: true,
       editEmployeeBtnClicked: false
-    });
+    }))
+    .catch(error => console.log(error) );
   }
 
   addEmployee(newEmployee) {
-    // push newEmployee object to state
     let firstName = newEmployee.firstName.value,
       lastName = newEmployee.lastName.value,
       job = newEmployee.jobTitle.value,
